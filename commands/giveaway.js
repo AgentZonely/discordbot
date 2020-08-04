0b31245a4a2d39cb58124b1e6f5766151a1b4324
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-05 01:26:42 +0300

    GIVEAWAY ADDED!

---
+++ commands/giveaway.js
@@ -0,0 +1,34 @@
+const {MessageEmbed, User} = require('discord.js')
+const ms = require('ms')
+module.exports={
+    name: 'giveaway',
+    description: 'Create a simple giveaway',
+    usage: '<time> <channel> <prize>',
+    category: 'fun',
+    run: async(bot,message,args)=>{
+        if(args[0]) return message.channel.send(`Usage: <time> <channel> <prize>`)
+        if(args[0].endsWith("d")&&!args[0].endsWith("h")&&!args[0].endsWith("m")) return message.channel.send(`Please specify your time in d (days), h (hours) or m (minutes)`)
+        if(isNaN(args[0][0])) return message.channel.send(`That is not a number!`)
+        let channel = message.mentions.channels.first()
+        if(!channel) return message.channel.send(`I could not find that channel in the server!`)
+        let prize = args.slice(2).join(" ")
+        if(!prize) return message.channel.send(`No prize specified!`)
+        message.channel.send(`Giveaway crated in ${channel}`)
+        let Embed = new MessageEmbed()
+        .setTitle(`New Giveaway`)
+        .setDescription(`The user ${message.author} is hosting a giveaway for the prize of **${prize}**`)
+        .setTimestamp(Date.now()+ms(args[0]))
+        .setColor(`BLUE`)
+      let m = await channel.send(Embed)
+      m.react("🎉")
+        setTimeout(() => {
+            if(m.reactions.cache.size<=1) return message.channel.send(`Not enough people reacted for me to end the giveaway :apusad:`)
+            if(m.reaction.cache.size<=0) return channel.send(`No one reacted, so I could not end the giveaway :apusad:`)
+            let winner = m.reaction.cache.get("🎉").users.cache.filter(u=>!u.bot)
+            channel.send(`Congrats ${winner} you have won **${prize}!!!**`)
+        }, ms(args[0]))
+
+    }
+}
+
+////?giveaway #h/d/m #giveaways code
