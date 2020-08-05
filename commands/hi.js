86c2adad2cf8ca9753ed797c20dc993c951e24dd
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-05 22:16:14 +0300

    hi

--- commands/hi.js
+++ commands/hi.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, mesage, args) => {
-    return mesage.channel.send("Hello :pepeyay:")
+    return mesage.channel.send("Hello peeps :D")
 }
 
 module.exports.config = {
