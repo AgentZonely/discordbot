ea7d662e40e5607e779b2da2b3600fee47aa71e1
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-06 16:17:01 +0300

    oof

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,22 +1,21 @@
 const Discord = require("discord.js");
 const botconfig = require("../botsettings.json");
-const ms = require("ms");
 
 module.exports.run = async (bot, mesage, args) => {
-    if(!args[0]) return Message.channel.send(`You did not specifiy your time!`);
+    if(!args[0]) return message.channel.send(`You did not specifiy your time!`);
     if(!args[0].endsWith("d") && !args[0].endsWith("h") && !args[0].endsWith("m"))
       return Message.channel.send(`The time needs to be specified in **days (d), hours (h), or minutes (m)**`);
       if (isNaN(args[0][0])) return Message.channel.send(`It must be a **number** you know that?`);
       
       let prize = args.slice(1).join(" ");
-      if(!prize) return Message.channel.send(`Are you seriously gonna create a giveaway with no **PRIZE?!** smh.`);
+      if(!prize) return message.channel.send(`Are you seriously gonna create a giveaway with no **PRIZE?!** smh.`);
 
       let Embed = new Discord.MessageEmbed()
       .setTitle(`New giveaway!`)
       .setDescription(`Hosted by: ${message.author}\nTime: ${args[0]}\nPrize: ${prize}`)
       .setTimestamp(Date.now() + ms(args[0]))
-      .setColor(`RANDOM`);
-    let m = await Message.channel.send(Embed);
+      .setColor(`BLUE`);
+    let m = await message.channel.send(Embed);
     m.react("🎉");
     setTimeout(() => {
         if(m.reaction.cache.get("🎉").count <= 1) {
@@ -24,7 +23,7 @@ module.exports.run = async (bot, mesage, args) => {
             .setColor("RED")
             .setDescription("No Winners")
             m.edit(embed)
-            return Message.channel.send(`This server is so lonely that no one entered and I could not pick a winner`);
+            return message.channel.send(`This server is so lonely that no one entered and I could not pick a winner`);
         }
 
         let winner = m.reactions.cache.get("🎉").users.cache.filter((b) => !b.bot).random();
