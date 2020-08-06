60d24c03f8b348b35f590dd694ea8885771d50ab
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-07 00:03:40 +0300

    loo

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const prefix = "?"
 const bot = new discord.Client()
 bot.commands = new discord.Collection()
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
-const config = require('./botsettings.json')
-const token = config.token;
-
 
 bot.aliases = new discord.Collection()
 for (const file of commandFiles) {
@@ -15,11 +11,6 @@ for (const file of commandFiles) {
   command.aliases.forEach(alias => bot.aliases.set(alias, command.name))
   bot.commands.set(command.name, command)
 }
-
-bot.categories = fs.readdirSync("./commandHandler");
-["commands"].forEach(handler => {
-    require(`./util/${handler}`)(bot);
-});
 
 bot.on("ready", () => {
   console.log("Im online")
