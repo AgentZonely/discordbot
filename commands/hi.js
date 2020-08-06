b368950d9514d942f38b0125bdfb272a8f2b566f
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-07 00:21:38 +0300

    Update hi.js

--- commands/hi.js
+++ commands/hi.js
@@ -1,13 +1,15 @@
 const Discord = require("discord.js");
-module.exports = {
+
+
+module.exports.run = async (bot, message, args) => {
+    return message.channel.send("Hello peeps :D")
+}
+
+module.exports.config = {
     name: "hi",
     description: "",
     usage: "?hi",
     accessableby: "Agents",
     aliases: ['hi']
-}
-
-module.exports.run = async (bot, message, args) => {
-    return message.channel.send("Hello peeps :D")
 }
 
