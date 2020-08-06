9a782fb61806b8c3d9826d86e0199fb15e93676a
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-06 22:27:37 +0300

    lel

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -3,40 +3,39 @@ const botconfig = require("../botsettings.json");
 const ms = require("ms");
 
 module.exports.run = async (bot, message, args) => {
-    if(!args[0]) return message.channel.send(`You did not specifiy your time!`);
-    if(!args[0].endsWith("d") && !args[0].endsWith("h") && !args[0].endsWith("m"));
-      message.channel.send(`**The time needs to be specified in **days (d), hours (h), or minutes (m)**`);
-      if (isNaN(args[0])) return message.channel.send(`It must be a **number** you know that?`);
-      
-      let prize = args.slice(1).join(" ");
-      if(!prize) return message.channel.send(`Are you seriously gonna create a giveaway with no **PRIZE?!** smh.`);
+    if (!args[0]) return message.channel.send(`You did not specify your time!`);
+    if (!args[0].endsWith("d") && !args[0].endsWith("h") && !args[0].endsWith("m"))
+      return message.channel.send(`The time needs to have days (d) or hours (h) or minutes (m)`);
+    if (isNaN(args[0][0])) return message.channel.send(`It must be a number you know that?`);
 
-      let Embed = new Discord.MessageEmbed()
+    let prize = args.slice(1).join(" ");
+    if (!prize) return message.channel.send(`No prize specified!`);
+  
+    let Embed = new discord.MessageEmbed()
       .setTitle(`New giveaway!`)
-      .setDescription(`Hosted by: ${message.author}\nTime: ${args[0]}\nPrize: ${prize}`)
+      .setDescription(`Host: ${message.author}\nTime: ${args[0]}\nPrize: ${prize}`)
       .setTimestamp(Date.now() + ms(args[0]))
       .setColor(`BLUE`);
     let m = await message.channel.send(Embed);
     m.react("🎉");
     setTimeout(() => {
-        if(m.reaction.cache.get("🎉").count <= 1) {
-            const embed = new Discord.MessageEmbed()
-            .setColor("RED")
-            .setDescription("No Winners")
-            m.edit(embed)
-            return message.channel.send(`This server is so lonely that no one entered and I could not pick a winner`);
-        }
-
-        let winner = m.reactions.cache.get("🎉").users.cache.filter((b) => !b.bot).random();
-
-        const embed = new Discord.MessageEmbed()
-        .setColor("GREEN")
-        .setDescription(`Winner: ${winner}`)
+      if (m.reactions.cache.get("🎉").count <= 1) {
+        const embed = new discord.MessageEmbed()
+        .setColor("RED")
+        .setDescription("No winners")
         m.edit(embed)
+        return message.channel.send(`Couldnt generate a winner as there is no one in that giveaway!`);
+      }
 
-        message.channel.send(`Congrats ${winner}! you have won the giveaway!`);
-    }), ms(args[0]);
+      let winner = m.reactions.cache.get("🎉").users.cache.filter((b) => !b.bot).random();
       
+      const embed = new discord.MessageEmbed()
+      .setColor("GREEN")
+      .setDescription(`Winner: ${winner}`)
+      m.edit(embed)
+      
+      message.channel.send(`The winnder of the giveaway is ${winner}`);
+    }, ms(args[0]));
 }
 
 module.exports.config = {
