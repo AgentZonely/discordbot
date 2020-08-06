38722f0a4f7b396879527d2f564fc10152e2d416
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-07 00:07:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
-const fs = require("fs")
-const discord = require("discord.js")
-const prefix = "?"
-const bot = new discord.Client()
-bot.commands = new discord.Collection()
+const Discord = require('discord.js');
+const botsettings = require('./botsettings.json');
 
+const bot = new Discord.Client({disableEveryone: true});
 
-bot.aliases = new discord.Collection()
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`)
-  command.aliases.forEach(alias => bot.aliases.set(alias, command.name))
-  bot.commands.set(command.name, command)
-}
-
-bot.on("ready", () => {
-  console.log("Im online")
+bot.on("guildMemberAdd", member => {
+    const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === '【😄】-welcome')
+    welcomeChannel.send (`Welcome! ${member} hope you enjoy your stay in **Team Agent**`)
 })
 
-bot.on('message' , async message => {
-  if(message.author.bot) return
-  
-  if(!message.content.startsWith(prefix)) return
-  
-  if(!message.member) message.member = await message.guild.fetchMember(message);
-  const args = message.content.slice(prefix.length).split(/ +/)
-  const command = args.shift().toLowerCase()
-  const cmd = bot.commands.get(command) || bot.commands.get(bot.aliases.get(command))
-  
-  if(cmd === null ) return
-  
-  if(cmd) cmd.run(bot, message, args)
-  if(!cmd) return
+require("./util/eventHandler")(bot)
+
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("[LOGS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
+
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let prefix = botsettings.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = message.content.substring(message.content.indexOf(' ')+1);
+
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+    if(commandfile) commandfile.run(bot,message,args)
+
 })
 
-bot.login("botsettings.token")
+bot.login(botsettings.token);
