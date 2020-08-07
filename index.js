57e6ce1219093ac0514cb56b8756afce754a08e7
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-07 22:51:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const botsettings = require('./botsettings.json');
 
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.config = {
-    api: process.botsettings.GOOGLE_API,
-  };
-
 bot.on("guildMemberAdd", member => {
     const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome')
     welcomeChannel.send (`Welcome! ${member}`)
