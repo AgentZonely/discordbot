8b4f3823b306244b991740f93cbb64041e83b56a
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-07 23:03:57 +0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
  const botconfig = require("../botsettings.json");
- const fs = require("YouTube");
 
 
     module.exports.run = async (bot, message, args) => {
