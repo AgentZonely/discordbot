6a30e37ac5e10f4260e22dc088aea65e95a72616
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-07 23:02:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const botsettings = require('./botsettings.json');
+const fs = require("YouTube");
 
 const bot = new Discord.Client({disableEveryone: true});
 
