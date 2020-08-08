768dc7be2be6b2bb12ed6d77e7cf0001112ea6e3
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-08 19:36:56 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,24 +3,24 @@ const botconfig = require("../botsettings.json");
 
 
 module.exports.run = async (bot, message, args) => {
-    let user;
+    let member;
   
-  if (message.mentions.users.first()) {
-    user = message.mentions.users.first();
+  if (message.mentions.members.first()) {
+    member = message.mentions.members.first();
   } else if (args[0]) {
-    user = message.guild.members.cache.get(args[0]).user;
+    member = message.guild.members.cache.get(args[0]).member;
   } else {
-    user = message.author;
+    member = message.author;
   }
   
-  let avatar = user.displayAvatarURL({size: 4096, dynamic: true});
+  let avatar = member.displayAvatarURL({size: 4096, dynamic: true});
   // 4096 is the new biggest size of the avatar.
   // Enabling the dynamic, when the user avatar was animated/GIF, it will result as a GIF format.
   // If it's not animated, it will result as a normal image format.
   
   const embed = new Discord.MessageEmbed()
-  .setTitle(`${user.tag} avatar`)
-  .setDescription(`[Avatar of **${user.tag}**](${avatar})`)
+  .setTitle(`${member.tag} avatar`)
+  .setDescription(`[Avatar of **${member.tag}**](${avatar})`)
   .setColor(0x1d1d1d)
   .setImage(avatar)
   
