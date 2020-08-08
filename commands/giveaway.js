bb0eac4a055d619547e36c0b21549e4b37808d8c
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-08 23:18:26 +0300

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -3,28 +3,16 @@ const botsettings = require("../botsettings.json");
 const ms = require('ms');
 
 module.exports.run = async (bot, message, args) => {
-    if(!args[0]) return Discord.MessageEmbed()
-    .setTitle('Please specify a time')
-    message.channel.send(embed);
+    if(!args[0]) return message.channel.send("Hey nerd! specify a time!")
 
-    if(!args[0].endsWith("d") &&!args[0].endsWith("h") &&!args[0].endsWith("m")) return let = new Discord.MessageEmbed()
-    .setTitle("bruh")
-    .setDescription("Atleast specify how long u want the giveaway to be!")
-    message.channel.send(embed);
-    if(isNaN(args[0][0])) return let = new Discord.MessageEmbed()
-    .setTitle("Whatever you just typed as the duration is NOT a number!")
-    message.channel.send(embed);
+    if(!args[0].endsWith("d") &&!args[0].endsWith("h") &&!args[0].endsWith("m")) return message.channel.send("Hey there nerd! the correct time format is **days (d), hours (h), or minutes (m)!**")
+    if(isNaN(args[0][0])) return message.channel.send("Whatever you just typed as the duration is NOT a number!")
     let channel = message.mentions.channels.first()
-    if(!channel) return let = new Discord.MessageEmbed()
-    .setTitle("You can't create a giveaway without **mentioning** a **channel** lol")
-    message.channel.send(embed);
+    if(!channel) return message.channel.send("You can't create a giveaway without **mentioning** a **channel** lol")
     let prize = args.slice(2).join(" ")
-    if(!prize) return let = new Discord.MessageEmbed()
-    .setTitle("Don't you wanna give out something _**NERD**_ (jk lol)")
-    message.channel.send(embed);
-    Discord.MessageEmbed()
-    .setTitle(`Giveaway created in **${channel}!**`)
-    message.channel.send(embed);
+    if(!prize) return message.channel.send("Don't you wanna give out something _**NERD**_ (jk lol)")
+    message.channel.send("Giveaway created in **${channel}!**")
+
     let embed = new Discord.MessageEmbed()
     .setTitle("Sup Nerds (jk)")
     .setDescription(`**${author.id}** is giving away **${prize}**`)
