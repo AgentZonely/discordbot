ed171f4ee97e39cc1bca2bf8533cbfaf9bcd0278
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-08 19:18:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-onst Discord = require('discord.js');
+const Discord = require('discord.js');
 const botsettings = require('./botsettings.json');
 
 const bot = new Discord.Client({disableEveryone: true});
