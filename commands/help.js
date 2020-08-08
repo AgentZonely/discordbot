aeae3283812d828b634c7a04ed4f3c839c7f4069
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-08 20:17:13 +0300

    g

--- commands/help.js
+++ commands/help.js
@@ -41,6 +41,23 @@ module.exports.run = async (bot,message,args) => {
 
         message.channel.send(embed);
     }}
+
+    if(usedCommand.has(message.author.id)){
+        const embed = new Discord.MessageEmbed()
+        .setColor("RED")
+        .setTitle("C'mon slow it down")
+        .setDescription("You can use another command in 3 more seconds")
+        message.channel.send(embed);
+    } else {
+        const embed = new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setTitle("The cooldown is over!")
+        message.channel.send(embed);
+        usedCommand.add(message.author.id);
+        setTimeout(() => {
+            usedCommand.delete(message.author.id);
+        }, 3000); //You can set the ammount of the cooldown here! Its Formated to Miliseconds.
+    }
 }
 
 module.exports.config = {
--- commands/hi.js
+++ commands/hi.js
@@ -3,7 +3,24 @@ const botconfig = require("../botsettings.json");
 
 
 module.exports.run = async (bot, message, args) => {
-    return message.channel.send("Hello peeps :D")
+    message.channel.send("Hello peeps :D")
+
+    if(usedCommand.has(message.author.id)){
+        const embed = new Discord.MessageEmbed()
+        .setColor("RED")
+        .setTitle("C'mon slow it down")
+        .setDescription("You can use another command in 3 more seconds")
+        message.channel.send(embed);
+    } else {
+        const embed = new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setTitle("The cooldown is over!")
+        message.channel.send(embed);
+        usedCommand.add(message.author.id);
+        setTimeout(() => {
+            usedCommand.delete(message.author.id);
+        }, 3000); //You can set the ammount of the cooldown here! Its Formated to Miliseconds.
+    }
 }
 
 module.exports.config = {
