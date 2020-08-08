2caa2dbf512cc81c12d6bd9155d69095b171bd31
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-08 20:58:41 +0300

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     if(!channel){
         return message.channel.send('Did you know that you have to mention a **channel** to create the poll in?!')
     }
-    let question = message.content.slice(bot.prefix.length+5+channel.id+3)
+    let question = message.content.slice(bot.prefix.length+5+channel.id.length+3)
     if(!question){
         return message.channel.send('Why do you want to create a poll without asking a question lmao.')
     }
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
     .setTitle('New Poll!')
     .setDescription(question)
     .setFooter(`Poll created by **${message.author.username}**`)
-  let message = await bot.channels.cache.get(channel.id).send(embed)
-   await message.react("👍")
-   await message.react("👎")
+  let msg = await bot.channels.cache.get(channel.id).send(embed)
+   await msg.react("👍")
+   await msg.react("👎")
 }
