2fa1f07c825a5efc2560ef7b252fcd3373f0d5f2
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-08 22:11:44 +0300

    pls work

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.on("message", async message => {
     let prefix = botsettings.prefix;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
-    let args = message.content.substring(message.content.indexOf(' ')+1);
+    const args = message.content.slice(prefix.length).split(/ +/)
 
     if(!message.content.startsWith(prefix)) return;
     let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
