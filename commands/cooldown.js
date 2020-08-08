af1a6916f7a08c641863242c82e42e96d5c22cf5
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-08 19:59:16 +0300

    Create cooldown.js

---
+++ commands/cooldown.js
@@ -0,0 +1,30 @@
+const { DiscordAPIError } = require("discord.js");
+const Discord = require("discord.js");
+const botconfig = require("../botsettings.json");
+
+const usedCommand = new Set();
+
+module.exports.run = async (bot, message, args) => {
+    if(usedCommand.has(message.author.id)){
+        const embed = new Discord.MessageEmbed()
+        .setColor("RED")
+        .setTitle("C'mon slow it down")
+        .setDescription("You can use another command in 3 more seconds")
+    } else {
+        const embed = new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setTitle("The cooldown is over!")
+        usedCommand.add(message.author.id);
+        setTimeout(() => {
+            usedCommand.delete(message.author.id);
+        }, 3000); //You can set the ammount of the cooldown here! Its Formated to Miliseconds.
+    }
+}
+
+module.exports.config = {
+    name: "cooldown",
+    description: "a Simple test of the Cooldown Command.",
+    usage: "?cooldown",
+    accessableby: "Members",
+    aliases: []
+}
