2202fc0fa907ef5addfb3136a4cc3dedadd3fdb0
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-08 23:10:44 +0300

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -7,19 +7,19 @@ module.exports.run = async (bot, message, args) => {
     .setTitle('Please specify a time')
     message.channel.send(embed);
 
-    if(!args[0].endsWith("d") &&!args[0].endsWith("h") &&!args[0].endsWith("m")) return Discord.MessageEmbed()
+    if(!args[0].endsWith("d") &&!args[0].endsWith("h") &&!args[0].endsWith("m")) return let = new Discord.MessageEmbed()
     .setTitle("bruh")
     .setDescription("Atleast specify how long u want the giveaway to be!")
     message.channel.send(embed);
-    if(isNaN(args[0][0])) return Discord.MessageEmbed()
+    if(isNaN(args[0][0])) return let = new Discord.MessageEmbed()
     .setTitle("Whatever you just typed as the duration is NOT a number!")
     message.channel.send(embed);
     let channel = message.mentions.channels.first()
-    if(!channel) return Discord.MessageEmbed()
+    if(!channel) return let = new Discord.MessageEmbed()
     .setTitle("You can't create a giveaway without **mentioning** a **channel** lol")
     message.channel.send(embed);
     let prize = args.slice(2).join(" ")
-    if(!prize) return Discord.MessageEmbed()
+    if(!prize) return let = new Discord.MessageEmbed()
     .setTitle("Don't you wanna give out something _**NERD**_ (jk lol)")
     message.channel.send(embed);
     Discord.MessageEmbed()
