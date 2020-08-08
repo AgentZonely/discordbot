38f7a294de14d0abb8915a04ee6c0b1ce50c2fcd
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-08 20:01:39 +0300

    Update cooldown.js

--- commands/cooldown.js
+++ commands/cooldown.js
@@ -10,10 +10,12 @@ module.exports.run = async (bot, message, args) => {
         .setColor("RED")
         .setTitle("C'mon slow it down")
         .setDescription("You can use another command in 3 more seconds")
+        message.channel.send(embed);
     } else {
         const embed = new Discord.MessageEmbed()
         .setColor("GREEN")
         .setTitle("The cooldown is over!")
+        message.channel.send(embed);
         usedCommand.add(message.author.id);
         setTimeout(() => {
             usedCommand.delete(message.author.id);
