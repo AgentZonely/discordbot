213f766c8c9a1beacad8aca8b06804c2a2e66873
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-09 19:12:28 +0300

    lol

--- commands/hi.js
+++ commands/hi.js
@@ -1,25 +1,11 @@
 const Discord = require("discord.js");
 const botsettings = require("../botsettings.json");
-const used = new Map();
-const Duration = new require('humanize-duration');
 
 
 module.exports.run = async (bot, message, args) => {
-    
-
-
-const cooldown = used.get(message.author.id);
-if(cooldown) {
-    const remaining = Duration(cooldown - Date.now(), { units: ['h', 'm'], round: true });
-    return messgae.reply(`C'mon slow it down, you can use another command in ${remaining}`).catch((err) => message.reply(`${err}`)); 
-}
-else {
     message.channel.send("Hello peeps :D")
-
-    used.set(message.author.id, Date.now() + 1000 * 60 * 60 * 24);
-    setTimeout(() => { used.delete(message.author.id), 1000 * 60 * 60 * 24});
 }
-};
+
 
 
 module.exports.config = {
---
+++ commands/poll.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const botsettings = require("../botsettings.json");
+
+module.exports.run = async (bot, message, args) => {
+    let pollChannel = message.channel.mentions.channels.first();
+    let pollDescription = args.slice(1).join('');
+
+    let embedPoll = new Discord.MessageEmbed()
+    .setTitle("New Poll!")
+    .setDescription(pollDescription)
+    .setColor('YELLOW')
+    let msgEmbed = await pollChannel.send(embedPoll);
+    await msgEmbed.react('👍')
+    await msgEmbed.react('👎')
+}
+
+
+
+module.exports.config = {
+    name: "Poll",
+    description: "Creates a poll",
+    usage: "?poll <channel id> <question>",
+}
