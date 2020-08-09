b846d61d483d003a898a63fc82d2b69b6f11358a
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-09 20:34:06 +0300

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, arg) => {
     if(!channel){
        return message.channel.send("Bruh, Mention a channel to create the poll in")
     }
-    let question = message.slice(1).join(" ")
+    let question = message.content.slice(bot.prefix.length+5+channel.id.length+3)
     if(!question){
         return message.channel.send('Why do u wanna create a poll without asking a question lol')
     } const embed = new Discord.MessageEmbed()
