46fbafd2b022b3055d7eb43774502e637c75d117
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-09 20:59:12 +0300

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -6,16 +6,16 @@ module.exports.run = async (bot, message, arg) => {
     if(!arg[1]) return message.channel.send("Hey nerd! specify a time!")
 
     if(!arg[1].endsWith("d") &&!arg[1].endsWith("h") &&!arg[1].endsWith("m")) 
-    if(isNaN(arg[1][1]) return message.channel.send("Whatever you just typed as the duration is NOT a number!")
+    if(isNaN(arg[1][1])) return message.channel.send("Whatever you just typed as the duration is NOT a number!")
     let channel = message.mentions.channels.first()
     if(!channel) return message.channel.send("You can't create a giveaway without **mentioning** a **channel** lol")
-    let prize = arg.slice(2).join(" ")
+    let prize = arg.slice(3).join(" ")
     if(!prize) return message.channel.send("Don't you wanna give out something _**NERD**_ (jk lol)")
     message.channel.send("Giveaway created in **${channel}!**")
 
     let embed = new Discord.MessageEmbed()
     .setTitle("Sup Nerds (jk)")
-    .setDescription(`**${author.id}** is giving away **${prize}**`)
+    .setDescription(`**${message.author.username}** is giving away **${prize}**`)
     .setTimestamp(Date.now()+ms(arg[1]))
     .setColor("YELLOW")
    let m = await message.channel.send(embed);
