ec4703648d3afb6501952c443a6939263cfc5288
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-09 20:55:39 +0300

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -3,10 +3,10 @@ const botsettings = require("../botsettings.json");
 const ms = require("ms");
 
 module.exports.run = async (bot, message, arg) => {
-    if(!arg[0]) return message.channel.send("Hey nerd! specify a time!")
+    if(!arg[1]) return message.channel.send("Hey nerd! specify a time!")
 
-    if(!arg[0].endsWith("d") &&!arg[0].endsWith("h") &&!arg[0].endsWith("m")) 
-    if(arg[0][0]) return message.channel.send("Whatever you just typed as the duration is NOT a number!")
+    if(!arg[1].endsWith("d") &&!arg[1].endsWith("h") &&!arg[1].endsWith("m")) 
+    if(arg[1][1]) return message.channel.send("Whatever you just typed as the duration is NOT a number!")
     let channel = message.mentions.channels.first()
     if(!channel) return message.channel.send("You can't create a giveaway without **mentioning** a **channel** lol")
     let prize = arg.slice(2).join(" ")
@@ -16,14 +16,14 @@ module.exports.run = async (bot, message, arg) => {
     let embed = new Discord.MessageEmbed()
     .setTitle("Sup Nerds (jk)")
     .setDescription(`**${author.id}** is giving away **${prize}**`)
-    .setTimestamp(Date.now()+ms(arg[0]))
+    .setTimestamp(Date.now()+ms(arg[1]))
     .setColor("YELLOW")
    let m = await message.channel.send(embed);
    m.react("🎉")
     .setTimeout(() => {
         let winner = m.reactions.cache.get("🎉").users.cache.filter(u=> !u.bot).random()
         channel.send(`Congratz **${winner}** you have won **${prize}!!!**`)
-    }, ms(arg[0]));
+    }, ms(arg[1]));
 
 
 }
