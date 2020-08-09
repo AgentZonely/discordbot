3ed21b469be65c2e44c72425f227c199c3827ed5
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-09 20:18:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ bot.on("message", async message => {
 
     if(!message.content.startsWith(prefix)) return;
     let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
-    if(commandfile) commandfile.run(bot,message,args)
+    if(commandfile) commandfile.run(bot,message,arg)
 
 })
 
