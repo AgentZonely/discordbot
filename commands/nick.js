f6a70f2259b9913dc37ae069bb4efb90bfe13da8
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-10 23:27:43 +0300

    yass

---
+++ commands/nick.js
@@ -0,0 +1,39 @@
+const { DiscordAPIError, MessageEmbed } = require("discord.js")
+const Discord = require("discord.js");
+const botsettings = require("../botsettings.json");
+
+module.exports.run = async (bot, message, arg) => {
+let perm = message.member.hasPermission("ADMINISTRATOR")
+
+let embed = new Discord.MessageEmbed()
+   .setTitle("ERROR")
+   .setDescription("**You don't have perms lol**")
+   .setColor("RED")
+if(!perm) return message.channel.send(embed)
+
+let user = message.mentions.members.first()
+let embed2 = new Discord.MessageEmbed()
+.setTitle("Mention a user to nick!")
+if(!user) return message.channel.send(embed2)
+
+let name = args.slice(1).join(" ")
+let embed3 = new Discord.MessageEmbed()
+.setTitle("Atleast give the name the user will be nicked to lol")
+if(!name) return message.channel.send(embed3)
+
+user.setNickname(name)
+
+let embed4 = new Discord.MessageEmbed()
+.setTitle(`Successfully nicked **${user.user.tag}** to **${name}!**`)
+
+message.channel.send(embed4)
+
+}
+
+module.exports.config = {
+    name: "nick",
+    description: "Nicks a user",
+    usage: "?nick <user> <name>",
+    accessableby: "Admins, Mods",
+    aliases: []
+}
