6e26aa2873116cde57df28c083ae8e3b6036d001
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-10 23:40:34 +0300

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const botsettings = require("../botsettings.json");
+
+
+module.exports.run = async (bot, message, arg) => {
+const user = message.mentions.users.first() || message.author;
+    const avatarEmbed = new discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setAuthor(user.tag)
+        .setImage(user.displayAvatarURL({dynamic: true , size: 2048}))
+        .setFooter(`Requested By: ${message.author.tag}`)
+        .setTimestamp()
+    message.channel.send(avatarEmbed);
+}
+
+module.exports.config = {
+    name: "avatar",
+    description: "Generates the avater for a user",
+    usage: "?avatar or ?avatar <user>",
+    accessableby: "Members",
+    aliases: ["None"]
+}
