316f6d5a4f69362600c42432bb273421861f62d3
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-11 21:36:30 +0300

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -4,31 +4,35 @@ const fs = require("fs");
 const money = require("../money.json");
 
 module.exports.run = async (bot, message, arg) => {
-    if(!arg[0]){
-        let user = message.author;
+
+    if(!arg[0]) {
+        var user = message.author;
     } else {
-        let user = message.mentions.users.first() || bot.users.cache.get(arg[0]);
+        var user = message.mentions.users.first() || bot.users.cache.get(arg[0]);
     }
 
-    if(!money[user.id]){
+    if(!money[user.id]) {
         money[user.id] = {
             name: bot.users.cache.get(user.id).tag,
             money: 0
         }
-        fs.writeFile("../money.json", JSON.stringify(money), (err) => {
+        fs.writeFile("./money.json", JSON.stringify(money), (err) => {
             if(err) console.log(err);
-        });
+        })
     }
 
-    return message.channel.send(`${bot.users.cache.get(user.id).username} has ${money[user.id].money}`);
+    let a = new Discord.MessageEmbed()
+        .setTitle("User's Balance")
+        .setColor("RANDOM")
+        .setDescription(`**${bot.users.cache.get(user.id).username}** has **${money[user.id].money.toLocaleString()} bills**.`)
+        return message.channel.send(a);
+
 }
 
-
-
-module.exports.config = {
+module.exports.help = {
     name: "balance",
-    description: "shows the amount of money u have",
-    usage: "?bal/?balance",
+    description: "",
+    usage: "?bal/balance",
     accessableby: "Members",
     aliases: ['bal']
 }
