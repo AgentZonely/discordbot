91d389561cb4138642d085df88cee10f2943d3cd
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-12 00:35:10 +0300

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -4,16 +4,11 @@ const db = require("quick.db");
 const ms = require("parse-ms");
 
 module.exports.run = async (bot, message, arg) => {
-    let user = message.mentions.users.first() || message.author.username
+    let user = message.mentions.users.first() || message.author
     let money = db.fetch(`money_${user.id}`)
-
-    let embedBal = new Discord.MessageEmbed()
-    .setTitle(`**${user}'s** balance`)
-    .setDescription(`Money: ${money}`)
-    .setColor("RANDOM")
     if (money === null) money = 0
 
-    message.channel.send(embedBal)
+    message.channel.send(`${user} has ${money} coins!`)
 }
 
 module.exports.config = {
