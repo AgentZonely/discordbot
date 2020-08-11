28692a21479f96baf18ca04a68c668ee7c4b4d56
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-12 00:33:17 +0300

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -9,11 +9,21 @@ module.exports.run = async (bot, message, arg) => {
 
    let daily = await db.fetch(`daily_${message.author.id}`)
    let time = ms(timeout - (Date.now() - daily));
-   
+
    let embedDaily = new Discord.MessageEmbed()
    .setTitle(`You have already collected your daily 1000 coins!, You can come back in **${time.hours}h ${time.minutes}m ${time.seconds}s**`)
    if (daily != null && timeout - (Date.now() - daily) > 0) {
        message.channel.send(embedDaily)
+   } else {
+       let embedDaily2 = new Discord.MessageEmbed()
+       .setTitle(`Daily`, message.author.displayAvatarURL)
+       .setColor("GREEN")
+       .setDescription("**Daily Rewards**")
+       .addField(`Collected`, amount)
+       message.channel.send(embedDaily2)
+
+       db.add(`money_${message.author.id}`, amount)
+       db.add(`daily_${message.author.id}`, Date.now())
    }
 }
 
