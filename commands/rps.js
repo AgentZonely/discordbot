5bf86d8831cfdf9d0361b1c4125d650b6a263374
AgentZonely <65575441+AgentZonely@users.noreply.github.com>
2020-08-16 14:56:33 +0300

    Update rps.js

--- commands/rps.js
+++ commands/rps.js
@@ -4,30 +4,30 @@ const botsettings = require("../botsettings.json");
 
 module.exports.run = async (bot, message, arg) => {
 let choices = ['rock', 'paper', 'scissors'];
-if (choices.includes((args[1]).toLowerCase())) {
+if (choices.includes((arg[1]).toLowerCase())) {
     let number = Math.floor(Math.random() * 3);
     if (number == 1) {
-        return message.channel.send('It was a tie, we both had ' + (args[1]).toLowerCase())
+        return message.channel.send('It was a tie, we both had ' + (arg[1]).toLowerCase())
     }
     if (number == 2) {
-        if ((args[1]).toLowerCase() == "rock") {
+        if ((arg[1]).toLowerCase() == "rock") {
             return message.channel.send('I won OMEGALUL (I chose paper).')
         }
-        if ((args[1]).toLowerCase() == "paper") {
+        if ((arg[1]).toLowerCase() == "paper") {
             return message.channel.send('I won OMEGALUL (I chose scissors).')
         }
-        if ((args[1]).toLowerCase() == "scissors") {
+        if ((arg[1]).toLowerCase() == "scissors") {
             return message.channel.send('I won OMEGALUL (I chose rock).')
         }
     }
     if (number == 0) {
-        if ((args[1]).toLowerCase() == "rock") {
+        if ((arg[1]).toLowerCase() == "rock") {
             return message.channel.send('I lost, *sad vibes* (I chose paper).')
         }
-        if ((args[1]).toLowerCase() == "paper") {
+        if ((arg[1]).toLowerCase() == "paper") {
             return message.channel.send('I lost, *sad vibes* (I chose rock)')
         }
-        if ((args[1]).toLowerCase() == "scissors") {
+        if ((arg[1]).toLowerCase() == "scissors") {
             return message.channel.send('I lost, *sad vibes* (I chose paper)')
         }
     }
